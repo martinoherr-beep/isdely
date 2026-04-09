0e82a62ee96154651fd8f6ca02391f8a92c2a6dc
martinoherr-beep <martinoherr@gmail.com>
2026-04-09 10:00:19 -0600

    app en escritorio

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,10 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('fetch', () => {});
+const CACHE_NAME = 'isdely-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Solo para cumplir el requisito de PWA
+  event.respondWith(fetch(event.request));
+});
