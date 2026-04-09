d4ef1767324e2e32243586450bb0e505bc96ab64
martinoherr-beep <martinoherr@gmail.com>
2026-04-09 12:33:32 -0600

    app en escritorio

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,20 @@
+const CACHE_NAME = 'isdely-v1';
+
+// Instalación: Forzar que el SW tome el control de inmediato
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
+// Activación: Limpiar versiones viejas si las hubiera
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+// Fetch: IMPORTANTE. Sin esto, Chrome no lo considera PWA instalable
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response('Sin conexión');
+    })
+  );
 });
